967192127da7f40997137ea37b5550f4c37d7dcd
dulleydoo <karem.jabar2015@gmail.com>
2022-09-04 19:53:00 -0400

    finished homework 3

---
+++ src/main/java/homework3/Main.java
@@ -0,0 +1,26 @@
+package homework3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pokemon p1 = new Pokemon("Pikachu",1 );
+        System.out.println(p1.level);
+        p1.attack();
+
+
+
+
+//        p1.name = "Pikachu";
+//        p1.level = 10;
+//
+//        Pokemon p2 = new Pokemon();
+//        p2.name = "Eevee";
+//        p2.level = 20;
+//
+//        System.out.println(p1.name + " " + p1.level);
+//
+//        p2.attack();
+
+
+    }
+}
